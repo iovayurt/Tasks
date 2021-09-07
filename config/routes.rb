7e41265a4c535ebb7cff84a0c11fd020460c75ed
Tahir Ovayurt <iovayurt@gmail.com>
2021-09-07 16:50:08 +0300

    task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task # form route; needs to be above show route
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # UPDATE EXISTING RESTAURANT
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task # our edit form
+  post 'tasks', to: 'tasks#create'
+  patch 'tasks/:id', to: 'tasks#update' # submit action for our edit form
+
+  # SHOW: SEE ONE RESTAURANT
+  # DELETE ONE RESTAURANT
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
